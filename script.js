3a047eaa7ab4995c6981798f6095f6bf54b3f95a
ShivaniF2000 <119884719+ShivaniF2000@users.noreply.github.com>
2023-04-19 19:04:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 //your code here!
+const infiList=document.getElementById("infi-list");
+const listItems=infiList.getElementById("li");
+const totalItems=10;
+let counter=0;
+function addMoreItems(){
+	//add more 2 items to the list
+	for(let i=0; i<2; i++){
+		const listItem= document.createElement("li");
+		listItem.textContemnt="List item" +(countter+i+1);
+		infiList.appendChild(listItem);
+	}
+	counter+=2;
+}
 
+//add initial list items
+for(let i=0;i<=totalItems;i++){
+	const listItem=document.createElement("li");
+	listList.textContent= "listItem"+i;
+	infiList.appendChild(listItem);
+	counter++;
+}
+
+//add event listener to detect when user reaches the end of the list
+infiList.addEventListener("scroll",function(){
+	if(
+		infiList.scrollTop+infiList.clientHeight>=infiList.scrollHeight-20)
+	    {
+		addMoreItems();
+		}
+});
